8f67308d2993459afc0c21d9bb8a74add4abd87f
Vishwanath Shinde <vishwanathshinde988@gmail.com>
2022-10-11 18:09:52 +0530

    first

---
+++ Assignment_1/B1.r
@@ -0,0 +1,14 @@
+rev_number <- function(n) {
+    rev1 = 0
+    sum = 0
+    r = 0
+    while(n > 0) {
+        r = n %% 10
+        sum = sum + r
+        rev1 = rev1 * 10 + r
+        n = n %/% 10
+    }
+    cat(paste("Reverse No is: ", rev1))
+    cat(paste("Sum of digit is: ", sum))
+}
+rev_number(123)
