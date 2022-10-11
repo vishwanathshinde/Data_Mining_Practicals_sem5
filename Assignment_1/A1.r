e32adc9b98a832b7a6731633258442a47ccdd315
Vishwanath Shinde <vishwanathshinde988@gmail.com>
2022-10-11 17:51:29 +0530

    first

---
+++ Assignment_1/A1.r
@@ -0,0 +1,10 @@
+v1 <- c(2, 4, 6)
+v2 <- c(1, 2, 3)
+
+cat("Addition of two vectors is: ", v1 + v2)
+
+cat("Multiplication of two vectors is: ", v1 * v2)
+
+cat("Division of two vectors is: ", v1 / v2)
+
+cat("Division of two vectors is: ", v2 / v1)
