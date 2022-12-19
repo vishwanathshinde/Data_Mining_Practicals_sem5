e132a1227d495312ea715239b5581580b513d8ea
Vishwanath Shinde <vishwanathshinde988@gmail.com>
2022-12-19 21:14:52 +0530

    first

--- bq1-1.py
+++ bq1-1.py
@@ -1,12 +1,9 @@
 # Assigning features and label variables
 from sklearn.naive_bayes import GaussianNB
 from sklearn import preprocessing
-weather = ['Sunny', 'Sunny', 'Overcast', 'Rainy', 'Rainy', 'Rainy', 'Overcast',
-           'Sunny', 'Sunny', 'Rainy', 'Sunny', 'Overcast', 'Overcast', 'Rainy']
-temp = ['Hot', 'Hot', 'Hot', 'Mild', 'Cool', 'Cool', 'Cool',
-        'Mild', 'Cool', 'Mild', 'Mild', 'Mild', 'Hot', 'Mild']
-play = ['No', 'No', 'Yes', 'Yes', 'Yes', 'No', 'Yes',
-        'No', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'No']
+weather = ['Sunny', 'Sunny', 'Overcast', 'Rainy', 'Rainy', 'Rainy', 'Overcast', 'Sunny', 'Sunny', 'Rainy', 'Sunny', 'Overcast', 'Overcast', 'Rainy']
+temp = ['Hot', 'Hot', 'Hot', 'Mild', 'Cool', 'Cool', 'Cool', 'Mild', 'Cool', 'Mild', 'Mild', 'Mild', 'Hot', 'Mild']
+play = ['No', 'No', 'Yes', 'Yes', 'Yes', 'No', 'Yes', 'No', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'No']
 # Import LabelEncoder
 # creating labelEncoder
 le = preprocessing.LabelEncoder()
@@ -20,9 +17,10 @@ print("Temp:", temp_encoded)
 print("Play:", label)
 # Combinig weather and temp into single listof tuples
 features = zip(wheather_encoded, temp_encoded)
+features = set(features)
 print(features)
 # Import Gaussian Naive Bayes model
-
+#2
 # Create a Gaussian Classifier
 model = GaussianNB()
 # Train the model using the training sets
