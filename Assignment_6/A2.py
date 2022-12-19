fed2a255a889f0a46a64b94a2a6a8ea94fbc7e50
Vishwanath Shinde <vishwanathshinde988@gmail.com>
2022-12-20 00:40:02 +0530

    first

---
+++ Assignment_6/A2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.cluster import AgglomerativeClustering
+import scipy.cluster.hierarchy as sch
+dataset = pd.read_csv(r'C:\Users\Vishwanath\Practicals---\Data_Mining_Practicals_sem5\Assignment_6\Customer.csv')
+X = dataset.iloc[:, [3, 4]].values
+dendrogram = sch.dendrogram(sch.linkage(X, method='ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
